1197f90e44b68ef255876db5cb5f7211f9c23ad3
emines0 <emin.sefa1@gmail.com>
2023-02-05 10:40:15 +0100

    Display tours

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
-};
+import React from 'react'
+import Tour from './Tour'
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>ours tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} /> //in the Tour component have access to all props that are in the object
+        })}
+      </div>
+    </section>
+  )
+}
 
-export default Tours;
+export default Tours
