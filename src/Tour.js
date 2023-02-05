6aceb40c476e45f09841fcd6993219451b6d8c30
emines0 <emin.sefa1@gmail.com>
2023-02-05 10:49:21 +0100

    toggle read more / show less info

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false)
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -9,7 +10,20 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className='tour-price'>${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {
+            readMore ? info : `${info.substring(0, 200)}...` // if readMore true display full info however if the readMore is false display only 200 characters of the info
+          }
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>{' '}
+          {/* 
+          
+          * Whatever is going to be in value read more change to opposite value
+          * If readMore is true button text will be 'show less' else 'read more' 
+          
+          */}
+        </p>
         <button className='delete-btn'>not interested</button>
       </footer>
     </article>
